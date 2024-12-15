4c15fc19eb3c33f2d298de19075e51fd3282f8e8
MartinXCVI <mdzg96@gmail.com>
2024-12-15 16:17:03 -0300

    Added elemental CORS configuration. // Installed dependencies: cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 // Routes imports
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
@@ -19,6 +20,11 @@ app.use(express.json())
 /* Middleware for parsing the incoming
 cookies sent from the client */
 app.use(cookieParser())
+
+app.use(cors({
+  origin: "http://localhost:5173", // React dev server
+  credentials: true, // Allows cookies to be sent
+}))
 
 /* Routes */
 app.use('/api/auth', authRoutes)
